b96aa3927f4d18353bbc2bec9a480f6c83f16f5e
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-09-20 19:18:41 -0300

    Initial structure.

---
+++ src/main/kotlin/br/com/fugisawa/itipasswordvalidationrest/ItiPasswordValidationRestKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fugisawa.itipasswordvalidationrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItiPasswordValidationRestKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItiPasswordValidationRestKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/fugisawa/itipasswordvalidationrest/ItiPasswordValidationRestKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fugisawa.itipasswordvalidationrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItiPasswordValidationRestKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
